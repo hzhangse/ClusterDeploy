f6f94166282aaeafa06768fe6fe96c1c93ab4cd0
hzhangse <2387362072@qq.com>
2015-06-12 13:06:30 +0800

    init project

---
+++ installer/Httpd/mod_proxy_balancer - Apache HTTP Server Version 2.4_files/show_comments.lua
@@ -0,0 +1,27 @@
+var last = 4012;
+var cached = false;
+var threads = new Array();
+threads[2113] = {"age":12183,"approved":1,"deleted":0,"flag":"","gravatar":"","id":2113,"name":"M","rating":0,"replyto":0,"text":"Under \"Balancer Manager\" you claim \"Please note that only Balancers defined outside of <Location ...> containers can be dynamically controlled by the Manager.\" But it appears that you also cannot manage Balancers defined in <Proxy ...> containers, either, which makes me sad.","trusted":0}; 
+threads[3766] = {"age":2035,"approved":1,"deleted":0,"flag":"","gravatar":"","id":3766,"name":"Victor Lewis","rating":0,"replyto":0,"text":"In documentation for mod_proxy_express it says mod_proxy_balancer \"also provides dynamic growth\" but there is no documentation here about how to do that.","trusted":0}; 
+var comments_disabled = false;
+var comments_requirevaliduser = false;
+var secQuestion = "What is four minus one?";
+var comments_site = "httpd";
+var comments_page = "http://httpd.apache.org/docs/2.4/mod/mod_proxy_balancer.html";
+var time_taken = 0.004448;
+var comments_stylesheet = "basic";
+var myName = unescape("");
+var myEmail = "";
+var rights = 0;
+var protocol = "https://";
+var siteURL = protocol + "comments.apache.org";
+var uid = '9d17f88a42b31b027793cf62850d3ba880597148';
+var old_school = 0;
+var c_c = "";
+var s = document.createElement('script');
+s.type = 'text/javascript';
+s.async = true;
+s.src = siteURL + '/builder.js';
+(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(s);
+
+    
